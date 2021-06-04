f3eefe80b7530e64b74ec262acb8f460b672fed3
atreat738 <atreat738@gmail.com>
2021-06-04 17:16:49 -0500

    The function playRound is now functional. The button is still just for show. Click it if you'd like.

--- Rock-Paper-Scissors/RPS.js
+++ Rock-Paper-Scissors/RPS.js
@@ -1,6 +1,6 @@
-const startGame = document.getElementById('StartGame');
+//const startGame = document.querySelector('button');
 
-
+//commputerPlay creates a random selection of rock, paper, or scissors. 
 function computerPlay() {
     let result = Math.floor(Math.random() * 3);
     if (result == 0) {   
@@ -14,29 +14,37 @@ function computerPlay() {
 
 
 const computerSelection = computerPlay();
-//const playerSelection = 'Rock';
 let playerSelection = prompt('Choose! Rock, Paper, Scissors!');
 
+//playRound asks user to type "rock, paper or scissors"
+//the input should not be case sensitive. 
+//A message will tell the user of the results in the console. 
+
 function playRound(playerSelection, computerSelection) {
-    console.log(`'You chose: '${playerSelection}`, `'Computer chose: ' ${computerSelection}`);
+    console.log(`You chose: ${playerSelection[0].toUpperCase()}${playerSelection.slice(1)}.`, `Computer chose:  ${computerSelection}.`);
     if (computerSelection.toUpperCase() == playerSelection.toUpperCase()) {
-        console.log('It\'s a tie! Who whoulda thunk!?');
+        console.log('It\'s a tie! Who woulda thunk!?');
+        return 'It\'s a tie! Who woulda thunk!?';
     }
     else if (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'Scissors') {
         console.log('You won! Rock beats scissors!');
+        return 'win';
     } else if (playerSelection.toUpperCase() == 'PAPER' && computerSelection == 'Rock') {
         console.log('You won! Paper covers rock!');
+        return 'win';
     }
     else if (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection == 'Paper') {
         console.log('You won! Scissors beats paper!');
-    } else {
-        return console.log('You lose! Try again!');
+        return 'win';
+    }else if(playerSelection.toUpperCase() !== 'Rock' && playerSelection.toUpperCase() !== 'Paper' && playerSelection.toUpperCase() !== 'Scissors') {
+        console.log('Invalid Input!')
+        return null;
+    }
+     else {
+        console.log('You lose! Try again!');
+        return 'lose';
     }
 }
 
-playRound(playerSelection, computerSelection);
-
-//button.onclick = playRound(playerSelection, computerSelection);
-//startGame.addEventListener("click", playRound(playerSelection, computerSelection));
 
 
