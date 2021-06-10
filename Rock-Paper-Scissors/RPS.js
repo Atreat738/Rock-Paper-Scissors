26d4689f8c9bdf13ece957b7427db6df861ade57
atreat738 <atreat738@gmail.com>
2021-06-10 13:12:09 -0500

    Added new feature to keep track of score. First to 5 wins! Button functionality will be added next.

--- Rock-Paper-Scissors/RPS.js
+++ Rock-Paper-Scissors/RPS.js
@@ -1,4 +1,4 @@
-//const startGame = document.querySelector('button');
+//const startGame = document.querySelector('button'); Will come back to this after learning more about DOM Manipulation.
 
 //commputerPlay creates a random selection of rock, paper, or scissors. 
 function computerPlay() {
@@ -12,19 +12,15 @@ function computerPlay() {
     }
 }
 
-
-const computerSelection = computerPlay();
-let playerSelection = prompt('Choose! Rock, Paper, Scissors!');
-
 //playRound asks user to type "rock, paper or scissors"
 //the input should not be case sensitive. 
 //A message will tell the user of the results in the console. 
 
 function playRound(playerSelection, computerSelection) {
-    console.log(`You chose: ${playerSelection[0].toUpperCase()}${playerSelection.slice(1)}.`, `Computer chose:  ${computerSelection}.`);
+    console.log(`You chose: ${playerSelection[0].toUpperCase()}${playerSelection.slice(1)} Computer chose: ${computerSelection}.`);
     if (computerSelection.toUpperCase() == playerSelection.toUpperCase()) {
         console.log('It\'s a tie! Who woulda thunk!?');
-        return 'It\'s a tie! Who woulda thunk!?';
+        return 'tie';
     }
     else if (playerSelection.toUpperCase() == 'ROCK' && computerSelection == 'Scissors') {
         console.log('You won! Rock beats scissors!');
@@ -32,12 +28,11 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection.toUpperCase() == 'PAPER' && computerSelection == 'Rock') {
         console.log('You won! Paper covers rock!');
         return 'win';
-    }
-    else if (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection == 'Paper') {
-        console.log('You won! Scissors beats paper!');
+    } else if (playerSelection.toUpperCase() == 'SCISSORS' && computerSelection == 'Paper') {
+        console.log('You won! Scissors cuts paper!');
         return 'win';
-    }else if(playerSelection.toUpperCase() !== 'Rock' && playerSelection.toUpperCase() !== 'Paper' && playerSelection.toUpperCase() !== 'Scissors') {
-        console.log('Invalid Input!')
+    } else if(playerSelection.toUpperCase() !== 'ROCK' && playerSelection.toUpperCase() !== 'PAPER' && playerSelection.toUpperCase() !== 'SCISSORS') {
+        console.log('Invalid Input!');
         return null;
     }
      else {
@@ -45,6 +40,41 @@ function playRound(playerSelection, computerSelection) {
         return 'lose';
     }
 }
+
+//game will begin keeping track of the scores
+//game will add a point to the winner's score each round
+//game will update the scoreboard
+let playerScore = 0;
+let computerScore = 0;
+
+
+function game() {
+    while (playerScore < 5 && computerScore < 5) {
+        
+        let playerSelection = prompt('Choose! Rock, Paper, Scissors!');
+        let computerSelection = computerPlay();
+        //playRound(playerSelection, computerSelection);
+        let roundResult = playRound(playerSelection, computerSelection);
+        if (roundResult === 'win') {
+            playerScore = ++playerScore;
+            console.log(`Your Score: ${playerScore}, Computer Score: ${computerScore}`);
+            if (playerScore === 5) {
+                console.log('You win!');
+                console.log(`Final Score: You: ${playerScore} Computer: ${computerScore}`);
+            }
+        } else if (roundResult !== 'win' && roundResult === 'lose') {
+            computerScore = ++computerScore;
+            console.log(`Your Score: ${playerScore}, Computer Score: ${computerScore}`);
+            if (computerScore === 5) {
+                console.log('You lose!');
+                console.log(`Final Score: You: ${playerScore} Computer: ${computerScore}`);
+            }
+        } else {
+            console.log(`The scores remain: ${playerScore} ${computerScore}`);           
+        }
+    }    
+}
+game();
 
 
 
